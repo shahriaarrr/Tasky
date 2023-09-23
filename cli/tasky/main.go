2d789d83b00ab2ac262c45011033970c5e439fc8
shahriaarrr <shahriaarrr@gmail.com>
2023-09-23 14:49:32 +0330

    add (add) flag

--- cli/tasky/main.go
+++ cli/tasky/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github/tasky"
+	"os"
+)
+
+const (
+	taskFile = ".tasky.jsone"
+)
 
 func main() {
-	fmt.Println("Hello")
+	add := flag.Bool("add", false, "add new task")
+	flag.Parse()
+
+	tasks := &tasky.Todos{}
+
+	if err := tasks.Load(taskFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		tasks.Add("task")
+		err := tasks.Store(taskFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
